cf08ecd476b44f28718fe8bed653ba500f1dc3d8
Raphael Ritz <raphael.ritz@gmail.com>
2016-07-01 15:34:48 +0200

    We can now add records to accounts and view them as JSON

--- src/eudat/accounting/server/browser/account.py
+++ src/eudat/accounting/server/browser/account.py
@@ -1,3 +1,10 @@
+
+import json
+import time
+
+from BTrees.IOBTree import IOBTree
+from persistent.mapping import PersistentMapping
+
 from Products.Five.browser import BrowserView
 
 class AccountView(BrowserView):
@@ -10,11 +17,38 @@ class AccountView(BrowserView):
         as key unless specifically provided
         The core elements need to be provided.
         Metadata is optional
-        Return the key under which teh record has been created
+        Return the key under which the record has been created
         """
-        return "Hello World from a record"
+        try:
+            data = self.context._data
+        except AttributeError:
+            data = self.context._data = IOBTree()
+        if key is None:
+            key = int(time.time())  # Seconds since 1.1.1970 0:00 UTC
+        record = PersistentMapping()
+        record['core'] = core
+        record['meta'] = meta
+        data[key] = record
+        return key
+
+    def listRecordKeys(self, n=10):
+        """List the last n records keys (default: 10)"""
+        n = int(n) + 1
+        data = self.context._data
+        result = list(self.context._data.keys())[:-n:-1]
+        return result
 
     def listRecords(self, n=10):
-        """List the last n records (default: 10)"""
-        # for now just return the objectIds
-        return self.context.objectIds()
+        """List the last n record items (keys and values)
+        as JSON"""
+        n = int(n) + 1
+        data = self.context._data
+        keys = list(self.context._data.keys())[:-n:-1]
+        result = []
+        for k in keys:
+            v = dict(data[k])
+            record = {}
+            record[k] = v
+            result.append(record)
+        self.request.response.setHeader('Content-type', 'application/json')
+        return json.dumps(result, indent=4)

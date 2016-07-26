24ae87ce47a84163c760b9f9253983172608fa89
Raphael Ritz <raphael.ritz@gmail.com>
2016-07-26 13:27:12 +0200

    Add browser view for account creation

--- src/eudat/accounting/server/browser/domain.py
+++ src/eudat/accounting/server/browser/domain.py
@@ -1,13 +1,23 @@
+from zope.interface import alsoProvides
 from Products.Five.browser import BrowserView
+from Products.BTreeFolder2.BTreeFolder2 import manage_addBTreeFolder
+from eudat.accounting.server.interfaces import IAccount
 
 class DomainView(BrowserView):
     """Browser views of a domain"""
 
-    def addAccount(self, id=None, owner=None):
+    def addAccount(self, id, title='', owner=None):
         """
         Add an account within a domain
         The id is usually the (P)ID of a resource
         Pass in the userid of the account owner
         If not provided the caller will own the account
         """
-        return "Hello World from an account"
+        manage_addBTreeFolder(self.context, id=id, title=title)
+        account = self.context[id]
+        alsoProvides(account, IAccount)
+        if owner is not None:
+            # careful: this works even if no user 'owner' exists
+            # TODO: add check here?
+            account.manage_addLocalRoles(owner, ['Owner'])
+        return "Account '%s (%s)' created." % (id, title

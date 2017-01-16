5f81120d72abcbeffe0574773c30f02eee059d45
Andreas Jung <info@zopyx.com>
2017-01-16 11:27:31 +0100

    some more checks, better error handling and proper HTTP status set

--- src/eudat/accounting/server/browser/domain.py
+++ src/eudat/accounting/server/browser/domain.py
@@ -13,12 +13,23 @@ class DomainView(BrowserView):
         Pass in the userid of the account owner
         If not provided the caller will own the account
         """
+
+        if id in self.context.objectIds():
+            self.request.response.setStatus(500)
+            return 'Folder "{}" already exists'.format(id)
+
+        if owner:
+            user = self.context.acl_users.getUser(owner)
+            if not user:
+                self.request.response.setStatus(500)
+                return 'No account for "{}" found - unable to set owner role'.format(owner)
+
+
         manage_addBTreeFolder(self.context, id=id, title=title)
         account = self.context[id]
         alsoProvides(account, IAccount)
         if owner is not None:
-            # careful: this works even if no user 'owner' exists
-            # TODO: add check here?
             account.manage_addLocalRoles(owner, ['Owner'])
-        return "Account '%s (%s)' created." % (id, title)
+        self.request.response.setStatus(201)
+        return 'Account {} ({}) created'.format(id, title)
 
